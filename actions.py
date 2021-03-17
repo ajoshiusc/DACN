9b01b24b47e9953948d9903cc3461911a22afe51
yanlong-sun <yanlongs@usc.edu>
2021-03-17 16:27:40 -0700

    unet only

--- actions.py
+++ actions.py
@@ -108,13 +108,13 @@ class Actions(object):
 
         # ——————————————  step：5  ——————————————#
         self.predictions = self.pred
-        self.decoded_predictions = tf.argmax(self.predictions, self.channel_axis, name='accuracy/decode_pred')   #----->
+        #self.decoded_predictions = tf.argmax(self.predictions, self.channel_axis, name='accuracy/decode_pred')   #----->
 
         gamma = 0.5
         high0 = tf.ones(self.annotations.shape, "int64")
         low0 = tf.zeros(self.annotations.shape, "int64")
         gamma0 = tf.ones(self.annotations.shape) * gamma
-        #self.decoded_predictions = tf.where(tf.greater_equal(self.predictions, gamma0), high0, low0)
+        self.decoded_predictions = tf.where(tf.greater_equal(self.predictions, gamma0), high0, low0)
 
         correct_prediction = tf.equal(self.annotations, self.decoded_predictions, name='accuracy/correct_pred')
         self.accuracy_op = tf.reduce_mean(tf.cast(correct_prediction, tf.float32, name='accuracy/cast'),
